9bb170ebc5ce67a42ae7b39d18c866a65e53d2b2
soetas <3228891558@qq.com>
2026-04-23 11:13:27 +0800

    v0.0.1

--- js/index.js
+++ js/index.js
@@ -22,15 +22,15 @@ function translate() {
 
   const aboutEnHtml = `
   <h1>👋🏻 Hello.</h1>
-  <p class="about-intro">I'm Yaxing Son, an open source enthusiast, and a web developer.</p>
+  <p class="about-intro">I am Sun Yaxing,  a web developer and open source enthusiast.</p>
   <p class="about-story">
-    I first encountered programming, especially front-end technologies, during 
-    my university years. The desire to share my work with others has been my main 
-    motivation for staying passionate about web development. At the same time, 
-    I truly enjoy the process of turning an idea into reality—it’s just so cool.
+    I became interested in programming during my senior year of college. After graduation, 
+    my first exposure to front-end development made me realize my strong interest in web 
+    technologies, and thus began my self-learning journey. I enjoy the process of quickly 
+    turning my ideas into projects and sharing them with others—it feels really cool.
   </p>
   <p class="about-activities">
-    In my free time, I write blog posts and occasionally stream programming content 
+    In my free time, I write blog posts on my <a href="">Blog</a> and occasionally stream programming content 
     on <a href="">Bilibili</a> and <a href="">YouTube</a>. Besides my professional work, I also contribute to open 
     source projects on <a href="">GitHub</a>. If you’ve benefited from my open source work, you can 
     support me financially through <a href="">GitHub Sponsors</a>. Of course, you’re also welcome to 
@@ -38,9 +38,8 @@ function translate() {
     life topics.
   </p>
   <p class="about-interests">
-    Beyond programming, I also enjoy design, photography, and pets. You can learn more about me 
-    through my <a href="">Blog</a>. If we share similar interests, feel free to 
-    reach out—let’s grab coffee or collaborate on something!
+    Outside of programming, I have a passion for design, photography, and pets. If you share 
+    any of these interests, I'd love to connect — whether over coffee or a potential collaboration.
   </p>
   <p>
     <span>&copy;2020-<span id="present">${getCurrentYear()}</span> Yaxing Son</span>
@@ -49,21 +48,21 @@ function translate() {
   `
   const aboutZhHtml = `
   <h1>👋🏻 你好。</h1>
-  <p class="about-intro">我是 Yaxing Son，一名开源爱好者，也是一名 Web 开发人员。</p>
+  <p class="about-intro">我是孙亚星，一名 Web 开发者和开源爱好者。</p>
   <p class="about-story">
-    我第一次接触编程，尤其是前端技术，是在大学期间。渴望与他人分享我的成果，一直是我保持 Web 
-    开发热情的主要动力。同时，我非常享受将想法变成现实的过程——这真的太酷了。
+    我在大四那年对编程产生了兴趣。毕业后，第一次接触前端开发让我意识到自己对 Web 技术有着强烈的兴趣，
+    我的自学之旅也由此开始。我很享受将自己的想法快速变成项目并与他人分享的过程——这感觉真的很酷。
   </p>
   <p class="about-activities">
-    闲暇时，我会撰写博客文章，并偶尔在 <a href="">哔哩哔哩</a> 和 <a href="">YouTube</a> 
+    闲暇时，我会在个人<a href="">博客</a>上写写文章，并偶尔在 <a href="">哔哩哔哩</a> 和 <a href="">YouTube</a> 
     上直播编程内容。除了专业工作外，我还在 <a href="">GitHub</a> 上为开源项目做贡献。如果您从
     我的开源工作中受益，您可以通过 <a href="">GitHub 赞助商</a> 为我提供经济支持。当然，也欢迎
     您在 <a href="">X</a> 和 <a href="">BlueSky</a> 上关注我，我有时会在那里分享关于科技
     产品和热门生活话题的帖子。
   </p>
   <p class="about-interests">
-    除了编程，我还喜欢设计、摄影和宠物。您可以通过我的<a href="">博客</a>了解更多关于我的信息。
-    如果我们有共同的兴趣，欢迎随时联系我们——一起喝杯咖啡，或者合作做点什么！
+    在编程之外，我对设计、摄影和宠物也充满热情。如果你对这些中的任何一项感兴趣，我都很乐意与你交流——无论是
+    喝杯咖啡，还是寻求潜在的合作机会。
   </p>
   <p>
     <span>&copy;2020-<span id="present">${getCurrentYear()}</span> Yaxing Son</span>
@@ -134,3 +133,4 @@ const savedTheme = localStorage.getItem('selectedTheme')
 if (savedTheme) {
   toggleTheme(savedTheme)
 }
+
